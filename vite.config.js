880e8c17e6b4de51d132e52c072e1c44bc1e4ace
Beren Yaşar <berenyasar20@gmail.com>
2026-07-31 14:24:42 +0300

    chore: add gh-pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Göreceli yolları destekleyerek tüm sunucularda hatasız çalışmasını sağlar
   plugins: [react()],
   server: {
-    host: true, // Yerel ağdaki (Wi-Fi) tüm cihazların erişimine açar
-    port: 3000  // Portu 3000 yapar
+    host: true,
+    port: 3000
   }
 })
